640027b6e9320d38859ce11704c1ca1fac702b61
ArjanJawahier <arjanjawahier@hotmail.com>
2021-02-24 22:45:20 +0100

    Improved the quality of the images by removing underscores from the characters. Underscores were bugged and flagged as if they had 0 brightness. Simply removing them will do. I also made the characters bigger by 2 pts, to fill up the space between the characters a bit more. This improves the resulting images a lot.

--- image_ascii_filter.py
+++ image_ascii_filter.py
@@ -16,40 +16,42 @@ def find_best_fitting_char(avg_brights, char_brights, row_block, col_block):
     for char, bright in char_brights[::-1]:
         if bright <= value:
             return char
+    return char
 
 def main():
     parser = argparse.ArgumentParser(description='Convert an image into ASCII art.')
     parser.add_argument('image_filename', help="The filename of the image you want to convert.")
-    parser.add_argument('char_size', type=int, help="The size of the ASCII characters of the new image.")
+    parser.add_argument('box_size', type=int, help="The size of the boxes the ASCII characters will go in.")
     args = parser.parse_args()
 
     with open("character_brightnesses.data", "rb") as filehandle:
-        char_brightness = pickle.load(filehandle)
+        char_brightness = pickle.load(filehandle)[1:] # Manually removing underscore (_ is bugged -> brightness of 0)
         max_char_brightness = char_brightness[-1][1]
 
-    let_size = (args.char_size, args.char_size)
+    box_size = (args.box_size, args.box_size)
     
     with Image.open(args.image_filename).convert('L') as original_img:
         original_imgarr = np.array(original_img)
         shape = original_imgarr.shape
-        num_vertical = shape[0]//let_size[0]
-        num_horizontal = shape[1]//let_size[1]
+        num_vertical = shape[0]//box_size[0]
+        num_horizontal = shape[1]//box_size[1]
         avg_brightness = np.zeros((num_vertical, num_horizontal))
         for row_block, col_block in product(range(num_vertical), range(num_horizontal)):
-            r, c = row_block * let_size[0], col_block * let_size[1]
+            r, c = row_block * box_size[0], col_block * box_size[1]
             avg_brightness[row_block, col_block] = average_brightness(
-                original_imgarr[r:r+let_size[0], c:c+let_size[1]], 
-                let_size
+                original_imgarr[r:r+box_size[0], c:c+box_size[1]], 
+                box_size
             ) / (255 / max_char_brightness)
 
     with Image.new('L', original_img.size) as ascii_img:
         draw = ImageDraw.Draw(ascii_img)
-        monospace_font = ImageFont.truetype("UbuntuMono-R.ttf", args.char_size)
+        monospace_font = ImageFont.truetype("UbuntuMono-R.ttf", args.box_size+2)
         for row_block, col_block in product(range(num_vertical), range(num_horizontal)):
             char = find_best_fitting_char(avg_brightness, char_brightness, row_block, col_block)
-            draw.text((col_block*let_size[1], row_block*let_size[0]), char, 255, font=monospace_font)
+            if char:
+                draw.text((col_block*box_size[1], row_block*box_size[0]), char, 255, font=monospace_font)
 
-        new_img_filename = args.image_filename.split(".")[0] + f"_ascii_{args.char_size}.jpg"
+        new_img_filename = args.image_filename.split(".")[0] + f"_ascii_{args.box_size}.jpg"
         ascii_img.save(new_img_filename, "JPEG")
 
 if __name__ == "__main__":
