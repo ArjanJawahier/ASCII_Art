cdf3c902fcf7e0a64f41e90b59e31fbee6d81f58
ArjanJawahier <arjanjawahier@hotmail.com>
2021-02-26 22:30:35 +0100

    Able to perform ASCII art in color as well now. If any average color is pure black, a RuntimeWarning is given because of a division by 0. This is handled further on in the pipeline.

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 This program converts any picture into ASCII art. Currently only works if the font UbuntuMono-R is installed (it is by default in Ubuntu).
 Install dependencies using the command `pip3 install -r requirements.txt`.
-Run with the command `python3 image_ascii_filter.py <img_file> <char_size>`.
+Run with the command `python3 image_ascii_filter.py <img_file> <char_size> [--color]`. If `--color` is specified, the ASCII characters are written in the average color of the neighbourhood of that ASCII character.
 
 ## Examples:
 ![scene12_original](images/scene12.png?raw=true)
 ![scene12_ascii5](images/scene12_ascii_5.jpg?raw=true)
-![scene12_ascii10](images/scene12_ascii_10.jpg?raw=true)
+![scene12_ascii10](images/scene12_ascii_10_color.jpg?raw=true)
 ![scene12_ascii15](images/scene12_ascii_15.jpg?raw=true)
--- image_ascii_filter.py
+++ image_ascii_filter.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 from itertools import product
 from ascii_brightness import average_brightness
+from color_functions import *
 
 def find_best_fitting_char(avg_brights, char_brights, row_block, col_block):
     """
@@ -18,40 +19,83 @@ def find_best_fitting_char(avg_brights, char_brights, row_block, col_block):
             return char
     return char
 
+
+def average_hsv(img, size, max_char_brightness):
+    arr = np.array(img)
+    w, h = size
+    total_color = np.zeros(3)
+    for r, c in product(range(h), range(w)):
+        total_color += arr[r, c]
+    average_color = total_color / (w*h)
+
+    avg_hsv = rgb_to_hsv(average_color)
+    if np.any(np.isnan(avg_hsv)):
+        avg_hsv = np.zeros(3)
+    else:
+        scaling_factor = max_char_brightness / 255
+        avg_hsv[2] *= scaling_factor
+
+    return avg_hsv
+
+
+def conversion(use_color):
+    if use_color:
+        return 'RGB'
+    else:
+        return 'L'
+
+def compute_filling_color(avg_hsv, row_block, col_block):
+    hsv = avg_hsv[row_block, col_block]
+    hsv[2] = 1.0
+    rgb = hsv_to_rgb(hsv) * 255
+    rgba_tuple = (int(rgb[0]), int(rgb[1]), int(rgb[2]), 255)
+    return rgba_tuple
+
+
 def main():
     parser = argparse.ArgumentParser(description='Convert an image into ASCII art.')
     parser.add_argument('image_filename', help="The filename of the image you want to convert.")
     parser.add_argument('box_size', type=int, help="The size of the boxes the ASCII characters will go in.")
+    parser.add_argument('--color', action="store_true", help="Whether to use colored ASCII characters.")
     args = parser.parse_args()
+
+    box_size = (args.box_size, args.box_size)
 
     with open("character_brightnesses.data", "rb") as filehandle:
         char_brightness = pickle.load(filehandle)[1:] # Manually removing underscore (_ is bugged -> brightness of 0)
         max_char_brightness = char_brightness[-1][1]
 
-    box_size = (args.box_size, args.box_size)
-    
-    with Image.open(args.image_filename).convert('L') as original_img:
+        
+    with Image.open(args.image_filename).convert(conversion(args.color)) as original_img:
         original_imgarr = np.array(original_img)
         shape = original_imgarr.shape
         num_vertical = shape[0]//box_size[0]
         num_horizontal = shape[1]//box_size[1]
-        avg_brightness = np.zeros((num_vertical, num_horizontal))
+        avg_hsv = np.zeros((num_vertical, num_horizontal, 3))
+
         for row_block, col_block in product(range(num_vertical), range(num_horizontal)):
             r, c = row_block * box_size[0], col_block * box_size[1]
-            avg_brightness[row_block, col_block] = average_brightness(
-                original_imgarr[r:r+box_size[0], c:c+box_size[1]], 
-                box_size
-            ) / (255 / max_char_brightness)
+            block = original_imgarr[r:r+box_size[0], c:c+box_size[1]]
+            avg_hsv[row_block, col_block] = average_hsv(block, box_size, max_char_brightness)
 
-    with Image.new('L', original_img.size) as ascii_img:
+
+    with Image.new(conversion(args.color), original_img.size) as ascii_img:
         draw = ImageDraw.Draw(ascii_img)
         monospace_font = ImageFont.truetype("UbuntuMono-R.ttf", args.box_size+2)
         for row_block, col_block in product(range(num_vertical), range(num_horizontal)):
-            char = find_best_fitting_char(avg_brightness, char_brightness, row_block, col_block)
+            char = find_best_fitting_char(avg_hsv[:, :, 2], char_brightness, row_block, col_block)
             if char:
-                draw.text((col_block*box_size[1], row_block*box_size[0]), char, 255, font=monospace_font)
+                if args.color:
+                    fill = compute_filling_color(avg_hsv, row_block, col_block)
+                else:
+                    fill = 255
+                draw.text((col_block*box_size[1], row_block*box_size[0]), char, fill=fill, font=monospace_font)
 
-        new_img_filename = args.image_filename.split(".")[0] + f"_ascii_{args.box_size}.jpg"
+
+        if args.color:
+            new_img_filename = args.image_filename.split(".")[0] + f"_ascii_{args.box_size}_color.jpg"
+        else:
+            new_img_filename = args.image_filename.split(".")[0] + f"_ascii_{args.box_size}.jpg"
         ascii_img.save(new_img_filename, "JPEG")
 
 if __name__ == "__main__":
